e0ee3a0cb9c33b890738a12e1aa26100e3060c28
nyp0821 <nyny0821@gmail.com>
2024-10-23 10:12:20 +0000

    ㄹ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 
-st.title("👋🏻 연수 실습 페이지~~~~~")
+st.title("👋🏻 연수 실습 페이지")
 st.subheader("저의 페이지에 오신 것을 환영합니다!")
-st.info("안녕하세요. !!!!!!박나연입니다.")
+st.info("안녕하세요. !!!!!~~~~!박나연입니다.")
 st.write("https://www.naver.com")
 
 st.link_button("저의 깃허브 페이지 바로가기", "https://github.com/nyp0821")
 
 # st.success("초록색 창")
 # st.error("빨간색 창")
-# st.info("파란색 창")
-# st.warning("노란색 창") # ctrl+/ : 주석처리
+st.info("파란색 창")
+st.warning("노란색 창") # ctrl+/ : 주석처리
 st.image("https://media.giphy.com/media/3o6gDWzmAzrpi5DQU8/giphy.gif?cid=790b7611zatw96cf98i40g6luhhzpcfse4r2x5nn4sf8wvgo&ep=v1_gifs_trending&rid=giphy.gif&ct=g", caption="Welcome to coding world") 
 st.video("https://www.youtube.com/watch?v=3Rz-M0U4TjM")
 st.image("data/스크린샷 2023-10-02 134322.png")

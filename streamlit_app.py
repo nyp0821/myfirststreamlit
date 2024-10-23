e13d3fe0b80697b689bfe8d3639c5757ff3bd0ea
nyp0821 <nyny0821@gmail.com>
2024-10-23 10:13:51 +0000

    제목 변경

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.link_button("저의 깃허브 페이지 바로가기", "https://github.com/ny
 
 # st.success("초록색 창")
 # st.error("빨간색 창")
-st.info("파란색 창")
+# st.info("파란색 창")
 st.warning("노란색 창") # ctrl+/ : 주석처리
 st.image("https://media.giphy.com/media/3o6gDWzmAzrpi5DQU8/giphy.gif?cid=790b7611zatw96cf98i40g6luhhzpcfse4r2x5nn4sf8wvgo&ep=v1_gifs_trending&rid=giphy.gif&ct=g", caption="Welcome to coding world") 
 st.video("https://www.youtube.com/watch?v=3Rz-M0U4TjM")
